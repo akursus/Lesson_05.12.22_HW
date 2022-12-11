c08c9e6e2ddbaa8ab39030b8199f2587b3a9acbe
akursus <ak_ursus@list.ru>
2022-12-11 22:59:23 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         //Task 1
         System.out.println("Задание 1");
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника — " + fullName);
 
@@ -12,6 +12,11 @@ public class Main {
         System.out.println("Задание 2");
         String fullName1 = fullName.toUpperCase();
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName1);
+
+        //Task 3
+        System.out.println("Задание 3");
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
 
 
     }
